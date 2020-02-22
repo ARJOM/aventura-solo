f8a738699a421c5ede4a34b0b34eae4e3bd1555b
Ricart <ricarteusuo11@gmail.com>
2020-02-22 16:28:22 -0300

    modelando classe responsável pelos passos de uma aventura

--- solo/models.py
+++ solo/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Step(models.Model):
+    id = models.IntegerField(primary_key=True)
+    content = models.TextField()
+    nextsteps = models.ManyToManyField('Step', blank=True)
+
+    def __str__(self):
+        return str(self.id)
